de6cbbabeee8a1a891972f7d37d3ad87d9e52802
IVKrylova <ivk.lis@yandex.ru>
2022-12-14 22:29:18 +0300

    fix: change color of figures

--- src/models/BoardClass.ts
+++ src/models/BoardClass.ts
@@ -32,8 +32,8 @@ export class BoardClass {
 
   private addBishops() {
     new Bishop(Colors.BLACK, this.getCell(2, 0));
-    new Bishop(Colors.WHITE, this.getCell(5, 0));
-    new Bishop(Colors.BLACK, this.getCell(2, 7));
+    new Bishop(Colors.BLACK, this.getCell(5, 0));
+    new Bishop(Colors.WHITE, this.getCell(2, 7));
     new Bishop(Colors.WHITE, this.getCell(5, 7));
   }
 
@@ -44,8 +44,8 @@ export class BoardClass {
 
   private addKnights() {
     new Knight(Colors.BLACK, this.getCell(1, 0));
-    new Knight(Colors.WHITE, this.getCell(6, 0));
-    new Knight(Colors.BLACK, this.getCell(1, 7));
+    new Knight(Colors.BLACK, this.getCell(6, 0));
+    new Knight(Colors.WHITE, this.getCell(1, 7));
     new Knight(Colors.WHITE, this.getCell(6, 7));
   }
 
@@ -63,8 +63,8 @@ export class BoardClass {
 
   private addRooks() {
     new Rook(Colors.BLACK, this.getCell(0, 0));
-    new Rook(Colors.WHITE, this.getCell(7, 0));
-    new Rook(Colors.BLACK, this.getCell(0, 7));
+    new Rook(Colors.BLACK, this.getCell(7, 0));
+    new Rook(Colors.WHITE, this.getCell(0, 7));
     new Rook(Colors.WHITE, this.getCell(7, 7));
   }
 
