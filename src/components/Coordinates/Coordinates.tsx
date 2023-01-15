16f0c486e853ca957707fcaeb39af6312b28c9c5
IVKrylova <ivk.lis@yandex.ru>
2023-01-15 13:46:48 +0300

    fix: add keys of coordinates

--- src/components/Coordinates/Coordinates.tsx
+++ src/components/Coordinates/Coordinates.tsx
@@ -9,7 +9,7 @@ interface CoordinatesProps {
 const Coordinates: FC<CoordinatesProps> = ({ coordinates, classModifier }) => {
   return (
     <ul className={`coordinates ${classModifier}`}>
-      {coordinates.map(el => <li className='coordinate'>{el}</li>)}
+      {coordinates.map((el, ind) => <li className='coordinate' key={ind}>{el}</li>)}
     </ul>
   );
 }
